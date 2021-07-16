4d8c4df0cbe9f423ec7ca1baf6e5a9ae886f8648
Ramez <ramezzormati1998@gmail.com>
2021-07-16 17:45:25 +0100

    responsive

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { InformationComponent } from './components/information/information.compo
 import { ProjectsComponent } from './components/projects/projects.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path: 'home' , component: HomeComponent},
   {path : 'contact' , component : ContactComponent},
   {path: 'projects',component: ProjectsComponent},
