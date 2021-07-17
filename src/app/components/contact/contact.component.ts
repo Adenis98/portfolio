7c399f8bf3c331fdf4c717dddf8b6996103c523c
Ramez <ramezzormati1998@gmail.com>
2021-07-17 15:03:47 +0100

    about

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,9 +1,15 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'] , 
+  animations: [
+    trigger('textOpen',[
+      transition('void => *',[style({opacity :0 ,transform : 'translateX(-7%)'}),animate('0.6s 0.2s ease-out')])
+    ])
+  ]
 })
 export class ContactComponent implements OnInit {
 
